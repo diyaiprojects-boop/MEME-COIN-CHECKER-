94f363ba8cce2dc682743708b4b5f8b2bef9bbe4
diyaiprojects-boop <diyaiprojects@gmail.com>
2025-09-14 16:56:29 +0100

    Update cn.ts

--- components/ui/cn.ts
+++ components/ui/cn.ts
@@ -1,3 +1,6 @@
-import { type ClassValue } from "class-variance-authority";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-export function cn(...inputs: ClassValue[]) { return twMerge(inputs.filter(Boolean).join(" ")); }
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(...inputs));
+}
